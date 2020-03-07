d66851339ee7761f605628f66f00704cbdb94c4e
okaaiiix <52967370+okaaiiix@users.noreply.github.com>
2020-03-07 21:56:43 +0800

    Updated app (draft)
    Just played around a bit

--- google-tech-challenge/src/App.js
+++ google-tech-challenge/src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
 import logo from './logo.svg';
+import Img from './3DC-logo.png';
 import './App.css';
+import Countdown from './countdown.js';
+import $ from 'jquery';
+import styled from 'styled-components';
+window.$ = $;
+
+const HoverText = styled.h1`
+  color: #000;
+  
+	:hover {
+		color: white;
+	}
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-body">
+        <div className>
+        <HoverText>
+          3DC
+        </HoverText>
+        </div>
+        <h2>
+          GTC
+        </h2>
+      </div>
+      <Countdown timeTillDate="05 18 2020, 9:00 am" timeFormat="MM DD YYYY, h:mm a" />
+      <div class="nc-main bg-cover bg-cc">
+
+
+			
+				<div class="bg-animation">
+					<div id='stars'></div>
+					<div id='stars2'></div>
+					<div id='stars3'></div>
+					<div id='stars4'></div>
+				</div>
+			</div>
     </div>
   );
 }
+
+$('h1').on("touchstart",function (){
+  //simply starts listening for touchstart - allows for hover state on touch devices
+});
 
 export default App;
